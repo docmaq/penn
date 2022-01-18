71133340986d8cf2966a5fd67140bf39670a58b7
Qian <82349191+docmaq@users.noreply.github.com>
2022-01-17 20:17:29 -0500

    Add src
    Add c src code.

---
+++ student.c
@@ -0,0 +1,62 @@
+#include "student.h"
+#include <stdbool.h>
+
+
+/* Lab- 1 Due by 11th Feb 2022
+   Make sure your code looks clean
+   Write comments with your code
+   Do not foget to 'push' your code github reglarly
+   */
+   
+
+// Takes an array of integers and the length of the array as input, and returns the smallest integer in the array
+int smallest(int array[], int length) {
+	// Function Body
+	return 0;
+	}
+	
+// Takes an array of integers and the length of the array as input, and returns the sum of the integers in the array.
+int sum(int array[], int length)  {
+	// Function Body
+	
+	return -1;
+	}
+	
+// Takes pointers to two integers and swaps the values of integers
+void swap(int *a, int *b) {
+	// Function Body
+	
+	}
+
+// Rotate values of integers
+void rotate(int *a, int *b, int *c){
+	// Function Body
+
+	}
+
+// Sorts an array in descending order 
+void sort(int array[], int length){
+	// Function Body
+
+	}
+	
+//Takes an array of integers and the length of the array as input and cubes  every prime element of the array
+void cube_primes(int array[], int length){
+	// Function Body
+
+	}
+	
+
+// Takes an array of integers and the length of the array as input and double every positive element of the array that is an Armstrong number. 
+void double_armstrongs(int array[], int length) {
+	// Function Body
+
+	}
+	
+//Take an array of integers and length of the arrays as input and negate every happy number of that array
+void negate_happy(int array[], int length){
+	// Function Body
+
+	}
+
+
